d3c7bd0a84a0e08e99beb23465bb7dcde8ee0b41
LiubomyrFadieiev <helplessmirek@gmail.com>
2025-05-16 18:10:12 +0200

    add user input to LDAPipeline

--- src/_public/preprocessingLDA.js
+++ src/_public/preprocessingLDA.js
@@ -16,8 +16,9 @@ export function preprocessLDA(boardgames) {
   ]);
 }
 
-function preprocessDataLDA(boardgames) {
+function preprocessDataLDA(boardgames, option = "ratings") {
 
+    /*
     return boardgames.map((boardgame) => [
     boardgame.year,
     boardgame.minplayers,
@@ -27,20 +28,45 @@ function preprocessDataLDA(boardgames) {
     boardgame.minage,
     preprocessedRating(boardgame.rating),
     // A lot of fields for mechanics (try later)
-  ]);
+    ]);
+    */
+    if (option === "ratings") {
+      
+      return boardgames.map((boardgame) => [
+        // Add 0/1 for every field 
+      ])
+    }
 
 }
 
-export function LDAPipeline(data) {
-  let classes = 0
+// Function for the whole LDA process 
+export function LDAPipeline(data, number_of_dims, classes_option = "ratings") {
+  let classes = generateClasses(data, classes_option)
 
   data = preprocessDataLDA(data)
   const X = druid.Matrix.from(data)
 
-  const reductionLDA = new druid.LDA(X, { labels: classes, d: 2 }) //2 dimensions, can use more.
+  const reductionLDA = new druid.LDA(X, { labels: classes, d: number_of_dims }) //2 dimensions, can use more.
   const result = reductionLDA.transform()
   
   return result
+}
+
+// Generates array of classes based on the data and selected option
+function generateClasses(data, classes_option) {
+  let classes = []
+  if (classes_option == "ratings") {
+    let middle = (border_values["rating"][1] - border_values["rating"][0])/2 + border_values["rating"][0] // a + (b-a)/2
+    for (let i = 0; i < data.boardgames.length; i++) {
+      if (data.boardgames[i].rating.rating <= middle) {
+        classes.push("low")
+      }
+      else {
+        classes.push("high")
+      }
+    }
+  }
+  return classes
 }
 
 // Takes two elements of the boardgames' rating and merges them into one metric.
@@ -63,12 +89,12 @@ function preprocessedRating(rating) {
 // Border values of categorical and quantitive attributes (x-a)/b
 // For data preprocessing and (maybe?) visualization
 const border_values = {
-  year: [1876, 2021],
-  max_player: [2, 8],
+  year: [2002, 2021],
+  max_player: [2, 7],
   min_player: [1, 3],
-  minplaytime: [5, 240],
-  maxplaytime: [20, 1000],
-  minage: [8, 17],
-  rating: [7.69721, 8.67527],
-  num_of_reviews: [5157, 96520],
+  minplaytime: [20, 240],
+  maxplaytime: [20, 480],
+  minage: [10, 14],
+  rating: [7.94204, 8.67527],
+  num_of_reviews: [7193, 85192],
 };
